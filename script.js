abc819ef0de3ccbb5016d4082a817229df819961
Huzaifa Rehan <93730388+MrFibonacc1@users.noreply.github.com>
2023-04-15 23:41:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// header scrolling effect
-$(window).on('scroll', function(){
-	if($(window).scrollTop()){
-      $('header').addClass('nav-show');
-		  
-	} 
-	else{
-		$('header').removeClass('nav-show');
-	}
-	   
+  
 })
 
 
